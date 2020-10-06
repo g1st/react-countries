113e6ea4eae3537ae7e8606092a36ab7023e3e01
g1st <gintstan@gmail.com>
2020-10-06 16:20:55 +0100

    Change icon's style

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,9 @@ const Header = ({ mode, handleChange }) => {
       <h1 className="title">Where in the world?</h1>
       <div className="mode-switch" onClick={handleChange}>
         {mode ? (
-          <i className="far fa-moon fa-1x theme-icon"></i>
+          <i className="fas fa-moon fa-1x theme-icon"></i>
         ) : (
-          <i className="far fa-sun fa-1x theme-icon"></i>
+          <i className="fas fa-sun fa-1x theme-icon"></i>
         )}
         {mode ? <span>Dark Mode</span> : <span>Light Mode</span>}
       </div>
